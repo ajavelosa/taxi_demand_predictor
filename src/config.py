7a62a56602ef21db0c3dc7635051c8f201a646c0
Anton J <antonjavelosa@gmail.com>
2024-06-01 09:56:35 +0800

    added N_FEATURES to config

--- src/config.py
+++ src/config.py
@@ -16,3 +16,6 @@ FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
 FEATURE_VIEW_NAME = 'time_series_hourly_feature_group'
 FEATURE_VIEW_VERSION = 1
+
+# number of historical values our model needs to generate predictions
+N_FEATURES = 24 * 28
