676746858b2435363e5c47d59c435e8917889a9e
Anton J <antonjavelosa@gmail.com>
2024-06-02 04:45:33 +0800

    corrected timedelta +/- for get_batch_data

--- src/inference.py
+++ src/inference.py
@@ -39,22 +39,24 @@ def load_batch_of_features_from_store(
     n_features = config.N_FEATURES
 
     # read time-series data from the feature store
-    fetch_data_to = current_date - timedelta(hours = 1) # one hour before
     fetch_data_from = current_date - timedelta(days  = 28) # four weeks ago
+    fetch_data_to = current_date - timedelta(hours = 1) # one hour before
     print(f'Fetching data from {fetch_data_from} to {fetch_data_to}')
+
     feature_view = feature_store.get_feature_view(
         name=config.FEATURE_VIEW_NAME,
         version=config.FEATURE_VIEW_VERSION
     )
     ts_data = feature_view.get_batch_data(
         start_time=fetch_data_from - timedelta(days=1),
-        end_time=fetch_data_to - timedelta(days=1),
+        end_time=fetch_data_to + timedelta(days=1),
     )
     ts_data = ts_data[ts_data.pickup_hour.between(fetch_data_from, fetch_data_to)]
 
     # validate that we are not missing data
-    location_ids = ts_data['location_id'].unique()
-    assert len(ts_data) == n_features*len(location_ids), 'Time-series data is incomplete'
+    location_ids = ts_data['pickup_location_id'].unique()
+    assert len(ts_data) == config.N_FEATURES * len(location_ids), \
+        "Time-series data is not complete. Make sure your feature pipeline is up and runnning."
 
     # sort data by location and time
     ts_data.sort_values(by=['pickup_location_id', 'pickup_hour'], inplace=True)
